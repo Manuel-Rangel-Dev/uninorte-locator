9a0e99b392058b48af4b1a68defb5a1098f89b3b
Sebastian R Pupo <srpupo@uninorte.edu.co>
2026-09-16 16:24:56 +0000

    Corrige puerto de dev a 8001 para no chocar con main

--- backend/api.py
+++ backend/api.py
@@ -52,4 +52,4 @@ def solo_hora():
 
 if __name__ == "__main__":
     # host="127.0.0.1": solo accesible localmente, jamás directo desde la web.
-    app.run(host="127.0.0.1", port=8000, debug=False)
+    app.run(host="127.0.0.1", port=8001, debug=False)
